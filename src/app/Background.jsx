58eb8569ff8345551fbd470bde0eb37c0f3e16ae
Mike Hacker <mhackersu@gmail.com>
2025-03-31 19:37:20 -0500

    update file location

--- src/app/Background.jsx
+++ src/app/Background.jsx
@@ -3,20 +3,9 @@ import React from "react";
 function Background() {
 	return (
 		<div className="app">
-		<section id="trigger1" data-scene="1" aria-label=""><a name="trigger1"></a>
-			<div className="up-down"></div>
-			<div className="intro-fg" id="intro">
-				<div id="logo_dct"></div>
-				<div id="logo_roadTrip"></div>
-				<div id="subtitle" class="subtitle"></div>
-				<div id="vehicle-choice">
-					<div id="vehicle">
-						<div className="arrow left-arrow"></div>
-						<div className="arrow right-arrow"></div>
-					</div>
-				</div>
-			</div>
-		</section>
+			<section>
+				<div className="up-down"></div>
+			</section>
 		</div>
 	);
 }
